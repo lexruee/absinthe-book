10153baf7b4a19dae9bff28f6e260fb99aeafedb
Alexander Rüedlinger <a.rueedlinger@gmail.com>
2019-08-04 05:31:41 +0200

    Add MenuItemsTest

---
+++ app/test/plate_slate_web/schema/query/menu_items_test.exs
@@ -0,0 +1,26 @@
+defmodule PlateSlateWeb.Schema.Query.MenuItemsTest do
+  use PlateSlateWeb.ConnCase, async: true
+
+  setup do
+    PlateSlate.Seeds.run()
+  end
+
+  @query """
+  {
+    menuItems {
+      name
+    }
+  }
+  """
+  test "menuItems field returns menu items" do
+    conn = build_conn()
+    conn = get(conn, "/api", query: @query)
+
+    assert %{"data" => %{"menuItems" => menu_items}} = json_response(conn, 200)
+    assert Enum.all?([
+        %{"name" => "Reuben"},
+        %{"name" => "Croque Monsieur"},
+        %{"name" => "Muffuletta"},
+    ], fn item -> item in menu_items end)
+  end
+end
