b58ef1cfd84e3905522bb81b84bb3c90a3a09486
Alexander Rüedlinger <a.rueedlinger@gmail.com>
2019-08-18 18:09:11 +0200

    Generate context ordering

---
+++ app/lib/plate_slate/ordering/order.ex
@@ -0,0 +1,22 @@
+defmodule PlateSlate.Ordering.Order do
+  use Ecto.Schema
+  import Ecto.Changeset
+  alias PlateSlate.Ordering.Order
+
+
+  schema "orders" do
+    field :customer_number, :integer
+    field :items, :map
+    field :ordered_at, :utc_datetime
+    field :state, :string
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(%Order{} = order, attrs) do
+    order
+    |> cast(attrs, [:customer_number, :items, :ordered_at, :state])
+    |> validate_required([:customer_number, :items, :ordered_at, :state])
+  end
+end
