c49cb3b57ebff84c529a96f313247abec9b8c224
Alexander Rüedlinger <a.rueedlinger@gmail.com>
2019-08-18 19:31:45 +0200

    Update Repo

--- app/lib/plate_slate/ordering/order.ex
+++ app/lib/plate_slate/ordering/order.ex
@@ -5,10 +5,11 @@ defmodule PlateSlate.Ordering.Order do
 
 
   schema "orders" do
-    field :customer_number, :integer
-    field :items, :map
-    field :ordered_at, :utc_datetime
-    field :state, :string
+    field :customer_number, :integer, read_after_writes: true
+    field :ordered_at, :utc_datetime, read_after_writes: true
+    field :state, :string, read_after_writes: true
+
+    embeds_many :items, PlateSlate.Ordering.Item
 
     timestamps()
   end
@@ -16,7 +17,7 @@ defmodule PlateSlate.Ordering.Order do
   @doc false
   def changeset(%Order{} = order, attrs) do
     order
-    |> cast(attrs, [:customer_number, :items, :ordered_at, :state])
-    |> validate_required([:customer_number, :items, :ordered_at, :state])
+    |> cast(attrs, [:customer_number, :ordered_at, :state])
+    |> cast_embed(:items)
   end
 end
